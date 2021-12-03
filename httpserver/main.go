b829393d80a3a16bbd46771704b1bdc51d96dbdb
mickey <qq523107430@163.com>
2021-12-03 10:44:26 +0800

    feat: module8 hoemwork

--- httpserver/main.go
+++ httpserver/main.go
@@ -4,26 +4,78 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"golang.org/x/sync/errgroup"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 // 有两种方式实现http server
 // 1. 通过 golang 自带的 net/http server 实现， net/http 包已经实现了 http包的解析
 // 2. 通过 golang 自带的 net/tcp 去实现， 这种方式需要自己解析http请求，自己拼装http响应
-var addr string
+
+var cfg string
+
+func init() {
+	flag.StringVar(&cfg, "c", "", "config path")
+	flag.Parse()
+}
+
+func initConfig(cfg string) error {
+	if cfg != "" {
+		viper.SetConfigFile(cfg)
+	} else {
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+		viper.AddConfigPath("conf")
+	}
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		logrus.Infof("config file changed: %s ", e.Name)
+		level, err := logrus.ParseLevel(viper.GetString("log.level"))
+		if err != nil {
+			logrus.Warn("日志配置出错")
+			return
+		}
+		logrus.SetLevel(level)
+		logrus.Infof("已经将日志等级改为 %v", viper.GetString("log.level"))
+	})
+	viper.WatchConfig()
+	return nil
+}
+
+func initLog() error {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	level, err := logrus.ParseLevel(viper.GetString("log.level"))
+	if err != nil {
+		return err
+	}
+	logrus.SetLevel(level)
+	return nil
+}
 
 func main() {
-	flag.StringVar(&addr, "a", ":9090", "http server addr")
-	flag.Parse()
+	err := initConfig(cfg)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = initLog()
+	if err != nil {
+		logrus.Fatal(err)
+	}
 
 	srv := &http.Server{
-		Addr:    addr,
+		Addr:    viper.GetString("addr"),
 		Handler: &Router{},
 	}
 
@@ -31,13 +83,13 @@ func main() {
 	g, errCtx := errgroup.WithContext(ctx)
 
 	g.Go(func() error {
-		log.Printf("strting http srever at address:%s \n", addr)
+		logrus.Infof("strting http srever at address:%s \n", viper.GetString("addr"))
 		return srv.ListenAndServe()
 	})
 
 	g.Go(func() error {
 		<-errCtx.Done()
-		log.Printf("stopping http server\n")
+		logrus.Infof("stopping http server\n")
 		return srv.Shutdown(errCtx)
 	})
 
@@ -46,7 +98,7 @@ func main() {
 		signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 		select {
 		case sig := <-done:
-			log.Printf("sig: %v\n", sig)
+			logrus.Infof("sig: %v\n", sig)
 			cancel()
 		case <-errCtx.Done():
 			return errCtx.Err()
@@ -55,10 +107,9 @@ func main() {
 	})
 
 	if err := g.Wait(); err != nil {
-		log.Printf("err: %v\n", err)
+		logrus.Infof("err: %v\n", err)
 	}
 }
-
 
 const (
 	HEALTH_CODE = 200
@@ -75,13 +126,16 @@ func (*Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	fmt.Printf("client ip:%s path: %s\n", getRealIP(r), path)
 	if path == "/healthz" {
-		fmt.Printf("http response code:200\n")
+		logrus.Infof("http response code:200\n")
+		fmt.Fprintln(w, HEALTH_CODE)
+	} else if path == "/debug" {
+		logrus.Debug("this is debug request")
 		fmt.Fprintln(w, HEALTH_CODE)
 	} else {
 		// handle other request
 		version := getOSEnv(VERSION, "")
 		w.Header().Set(VERSION, version)
-		fmt.Printf("http response code:200\n")
+		logrus.Infof("http response code:200\n")
 		fmt.Fprintln(w, "every thing's be ok")
 	}
 }
@@ -97,4 +151,3 @@ func getOSEnv(key, defaultValue string) string {
 func getRealIP(r *http.Request) string {
 	return r.RemoteAddr
 }
-
